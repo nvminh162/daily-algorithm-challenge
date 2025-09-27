26a3baeaffcbfc8dd54e86f99ae5f3a84bc9150e
nvminh162 <nvminh162@gmail.com>
2025-09-27 20:15:17 +0700

    feat: add CaesarCipher implementation with encryption and decryption methods

---
+++ playground/cryptography/CaesarCipher.java
@@ -0,0 +1,50 @@
+package playground.cryptography;
+
+public class CaesarCipher {
+    public static final String alpha = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String encrypt(String message, int shiftKey) {
+        message = message.toLowerCase();
+        String cipherText = "";
+        for (int ii = 0; ii < message.length(); ii++) {
+            int charPosition = alpha.indexOf(message.charAt(ii));
+            int keyVal = (shiftKey + charPosition) % 26;
+            char replaceVal = alpha.charAt(keyVal);
+            cipherText += replaceVal;
+        }
+        return cipherText;
+    }
+
+    public static String decrypt(String cipherText, int shiftKey) {
+        cipherText = cipherText.toLowerCase();
+        String message = "";
+        for (int ii = 0; ii < cipherText.length(); ii++) {
+            int charPosition = alpha.indexOf(cipherText.charAt(ii));
+            int keyVal = (charPosition - shiftKey) % 26;
+            if (keyVal < 0) {
+                keyVal = alpha.length() + keyVal;
+            }
+            char replaceVal = alpha.charAt(keyVal);
+            message += replaceVal;
+        }
+        return message;
+    }
+
+    public static void main(String[] args) {
+        String plainText = "sofarsogood";
+        String encode = "xtkfwxtltti";
+        int s = 5;
+
+        System.out.println("----------------");
+
+        System.out.println("encrypt: ");
+        System.out.println("Text  : " + plainText);
+        System.out.println("Cipher: " + encrypt(plainText, s));
+
+        System.out.println("----------------");
+
+        System.out.println("decrypt: ");
+        System.out.println("Text : " + encode);
+        System.out.println("Cipher: " + decrypt(encode, s));
+    }
+}
